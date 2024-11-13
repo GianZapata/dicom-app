80919a65cc343c2fc1238ee2a6761283a1d01f76
Gian <gzapata@acsyt.com>
2024-11-12 23:18:28 -0600

    Agregar soporte para archivos DICOM y conversión a PNG en la API de adjuntos

--- src/app/api/attachments/[id]/route.ts
+++ src/app/api/attachments/[id]/route.ts
@@ -1,6 +1,8 @@
 import { findOneAttachmentById } from "@/actions/get-attachments.action"
 import { NextResponse } from "next/server";
 import fs from 'fs';
+import dcmjs from 'dcmjs';
+import { PNG } from 'pngjs';
 
 export async function GET(
    request: Request,
@@ -11,7 +13,6 @@ export async function GET(
    const attachment = await findOneAttachmentById(Number(id))
    if(!attachment) return new Response(null, { status: 404 })
 
-   
    const fileName = `${attachment.name}`
    const filePath = `${attachment.path}/${fileName}` // storage/attachments/fileName.ext
    const fullPath = `${process.cwd()}/${filePath}`
@@ -19,7 +20,12 @@ export async function GET(
    try {
       const fileContent = fs.readFileSync(fullPath);
 
-      // Devuelve la respuesta con el archivo y los encabezados adecuados
+      const dicomData = dcmjs.data.DicomMessage.readFile(fileContent.buffer);
+
+      const patientName = dicomData.dict['00100010']?.Value?.[0] || 'Desconocido';
+      const studyDate = dicomData.dict['00080020']?.Value?.[0];
+      const modality = dicomData.dict['00080060']?.Value?.[0];
+
       return new NextResponse(fileContent, {
          status: 200,
          headers: {
@@ -27,6 +33,54 @@ export async function GET(
             'Content-Disposition': `attachment; filename="${fileName}"`,
          },
       });
+
+      if ('7FE00010' in dicomData.dict) {
+         const pixelDataElement = dicomData.dict['7FE00010'];
+         const pixelData = pixelDataElement.Value[0];
+
+         const rows = parseInt(dicomData.dict['00280010'].Value[0], 10);
+         const cols = parseInt(dicomData.dict['00280011'].Value[0], 10);
+         const bitsAllocated = parseInt(dicomData.dict['00280100'].Value[0], 10);
+
+         let pixelDataArray;
+         if (bitsAllocated === 16) {
+            pixelDataArray = new Uint16Array(pixelData.buffer);
+         } else {
+            pixelDataArray = new Uint8Array(pixelData.buffer);
+         }
+
+         const pixelData8 = new Uint8Array(pixelDataArray.length);
+         const maxVal = Math.max(...pixelDataArray);
+         const minVal = Math.min(...pixelDataArray);
+
+         for (let i = 0; i < pixelDataArray.length; i++) {
+            pixelData8[i] = ((pixelDataArray[i] - minVal) * 255) / (maxVal - minVal);
+         }
+
+      
+         // Crea una imagen PNG
+         const png = new PNG({ width: cols, height: rows, colorType: 0 });
+
+         // Convierte la imagen PNG a un buffer
+         const pngBuffer = PNG.sync.write(png);
+
+         fs.writeFileSync('test_image.png', pngBuffer);
+
+
+         // Devuelve la imagen PNG al cliente
+         return new NextResponse(pngBuffer, {
+            status: 200,
+            headers: {
+               'Content-Type': 'image/png',
+            },
+         });
+      } else {
+         const metadata = {
+            patientName,
+            studyDate,
+            modality,
+         };
+      }
    } catch (error) {
       console.error('Error al leer el archivo:', error);
       return new Response('Error al leer el archivo', { status: 500 });
